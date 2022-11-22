ccf4a9b56e825fde8fbfad8509a3d4edddb899a3
Sagiri <sagiri.ikenaga@ryerson.ca>
2022-11-21 20:25:33 -0500

    styled portfolio

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -5,9 +5,9 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <a href="https://github.com/sagirikenaga" target="_blank" rel="noopener noreferrer">Github</a>
-      <a href="https://linkedin.com/in/sikenaga" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      <a href="https://stackoverflow.com/users/20567310/sagiri-ikenaga" target="_blank" rel="noopener noreferrer">Stack</a>
+      <a href="https://github.com/sagirikenaga" target="_blank" rel="noopener noreferrer"><i className='fa fa-2x fa-github'></i></a>
+      <a href="https://linkedin.com/in/sikenaga" target="_blank" rel="noopener noreferrer"><i className='fa fa-2x fa-linkedin'></i></a>
+      <a href="https://stackoverflow.com/users/20567310/sagiri-ikenaga" target="_blank" rel="noopener noreferrer"><i className='fa fa-2x fa-stack-overflow'></i></a>
     </footer>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,56 +1,114 @@
-import React from 'react';
-import '../styles/PortfolioPage.css';
-import project1 from '../../images/project1.png';
-import project2 from '../../images/project2.png';
-import module3 from '../../images/module3.png';
-import module4 from '../../images/module4.png';
-import module5 from '../../images/module5.png';
-import module11 from '../../images/module11.png';
-import module19 from '../../images/module19.png';
+import React from "react";
+import "../styles/PortfolioPage.css";
+import project1 from "../../images/project1.png";
+import project2 from "../../images/project2.png";
+import module3 from "../../images/module3.png";
+import module4 from "../../images/module4.png";
+import module11 from "../../images/module11.png";
+import module19 from "../../images/module19.png";
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <div>
-            <article className="box">
-                <a href="https://sagirikenaga.github.io/whats-the-word/">
-                  <img src={project1} alt="'What's the Word?' Game App" height="400"></img>
-                </a>
-            </article>
-        </div>
-        <div>
-            <article className="box">
-                <a href="https://petplace2141.herokuapp.com/">
-                    <img src={project2} alt="Pet's Place Site" height="400"></img>
-                </a>
-            </article>
-            <article className="box">
-                <a href="https://github.com/sagirikenaga/random-password-generator">
-                    <img src={module3} alt="Random Password Generator App" height="400"></img>
-                </a>
-            </article>
-            <article className="box">
-                <a href="https://sagirikenaga.github.io/coding-quiz/">
-                  <img src={module4} alt="Coding Quiz App" height="400"></img>
-                </a>
-            </article>
-            <article className="box">
-                <a href="https://sagirikenaga.github.io/work-day-scheduler/">
-                    <img src={module5} alt="Work Day Scheduler App" height="400"></img>
-                </a>
-            </article>
-            <article className="box">
-                <a href="https://si-note-taker.herokuapp.com/">
-                    <img src={module11} alt="Note Taker App" height="400"></img>
-                </a>
-            </article>
-            <article className="box">
-                <a href="https://si-note-editor.herokuapp.com/">
-                    <img src={module19} alt="Text Editor App" height="400"></img>
-                </a>
-            </article>
-        </div>
+      <div id="portfolio-box">
+      <div className="project">
+        <a href="https://sagirikenaga.github.io/whats-the-word/">
+          <img
+            src={project1}
+            alt="'What's the Word?' Game App"
+            height="400"
+            className="portfolio-img"
+          ></img>
+        </a>
+        <a
+          className="github-a"
+          href="https://github.com/sagirikenaga/whats-the-word"
+        >
+          Github Link
+        </a>
+      </div>
+      <div className="project">
+        <a href="https://petplace2141.herokuapp.com/">
+          <img
+            src={project2}
+            alt="Pet's Place Site"
+            height="400"
+            className="portfolio-img"
+          ></img>
+        </a>
+        <a
+          className="github-a"
+          href="https://github.com/Favour01216/Adoption-site"
+        >
+          GitHub Link
+        </a>
+      </div>
+      <div className="project">
+        <a href="https://github.com/sagirikenaga/random-password-generator">
+          <img
+            src={module3}
+            alt="Random Password Generator App"
+            height="400"
+            className="portfolio-img"
+          ></img>
+        </a>
+        <a
+          className="github-a"
+          href="https://github.com/sagirikenaga/random-password-generator"
+        >
+          GitHub Link
+        </a>
+      </div>
+      <div className="project">
+        <a href="https://sagirikenaga.github.io/coding-quiz/">
+          <img
+            src={module4}
+            alt="Coding Quiz App"
+            height="400"
+            className="portfolio-img"
+          ></img>
+        </a>
+        <a
+          className="github-a"
+          href="https://github.com/sagirikenaga/coding-quiz"
+        >
+          GitHub Link
+        </a>
+      </div>
+      <div className="project">
+        <a href="https://si-note-taker.herokuapp.com/">
+          <img
+            src={module11}
+            alt="Note Taker App"
+            height="400"
+            className="portfolio-img"
+          ></img>
+        </a>
+        <a
+          className="github-a"
+          href="https://github.com/sagirikenaga/note-taker"
+        >
+          GitHub Link
+        </a>
+      </div>
+      <div className="project">
+        <a href="https://si-note-editor.herokuapp.com/">
+          <img
+            src={module19}
+            alt="Text Editor App"
+            height="400"
+            className="portfolio-img"
+          ></img>
+        </a>
+        <a
+          className="github-a"
+          href="https://github.com/sagirikenaga/text-editor"
+        >
+          GitHub Link
+        </a>
+      </div>
+    </div>
     </div>
   );
 }
