932ceaa3f63cc660c0928ff733d9ed68bb7f453d
Sagiri <sagiri.ikenaga@ryerson.ca>
2022-11-21 23:46:52 -0500

    completed contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,36 +1,69 @@
-import React from "react";
-import '../styles/ContactPage.css';
+import React, { useState } from "react";
+import "../styles/ContactPage.css";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(false);
+
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      name.length === 0 ||
+      email.length === 0 ||
+      message.length === 0 ||
+      email !== isValidEmail(email)
+    ) {
+      setError(true);
+    }
+  };
+
   return (
-    <div className="container">
+    <form className="container" onSubmit={handleSubmit}>
       <div class="row">
         <h1 className="contact-header">Contact Me:</h1>
       </div>
       <div class="row input-container">
-        <div class="col-xs-12">
+        <div>
           <div class="styled-input wide">
-            <input type="text" required />
-            <label>Name</label>
+            <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+            {error && name.length <= 0 ? (
+              <label>Name cannot be empty.</label>
+            ) : (
+              "Name*"
+            )}
           </div>
         </div>
-        <div class="col-md-6 col-sm-12">
+        <div>
           <div class="styled-input">
-            <input type="text" required />
-            <label>Email</label>
+            <input type="email" placeholder="Email Address" onChange={(e) => setEmail(e.target.value)} />
+            {error && email.length <= 0 ? (
+              <label>Please enter a valid email address.</label>
+            ) : (
+              "Email*"
+            )}
           </div>
         </div>
-        <div class="col-xs-12">
+        <div>
           <div class="styled-input wide">
-            <textarea required></textarea>
-            <label>Message</label>
+            <textarea placeholder="Write Message Here" onChange={(e) => setMessage(e.target.value)}></textarea>
+            {error && message.length <= 0 ? (
+              <label>Message cannot be empty.</label>
+            ) : (
+              "Message*"
+            )}
           </div>
         </div>
-        <div class="col-xs-12">
-          <div class="btn-lrg submit-btn">Send Message</div>
+        <div>
+          <button class="btn-lrg submit-btn">Send Message</button>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
@@ -39,4 +72,4 @@ export default function Contact() {
 
 // NEED TO CREATE A CONTACT FORM WITH NAME, EMAIL AND MESSAGE
 // FIELD MUST BE REQUIRED
-// MUST BE VALID EMAIL ADDRESS
+// MUST BE VALID EMAIL ADDRESSi
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,10 +6,11 @@ export default function Home() {
     <div>
       <h1>About Me</h1>
       <img src={myphoto} alt="Sagiri" height="400"></img>
+      <div id="textbox">
       <p>
-      Hello! My name is Sagiri (pronounced suh-gear-ee) Ikenaga and I am a student in the Full Stack Developer coding Bootcamp at U of T SCS. 
+      Hello! My name is Sagiri (pronounced suh-gear-ee) Ikenaga and I am a student in the Full Stack Developer coding Bootcamp at University of Toronto SCS. 
       I completed a Bachelor of Science in Neuroscience and I am currently finishing up a Master of Engineering in Biomedical Engineering. 
-      I am passionate about the growing technology and software field, and hope to eventually integrate my work in these fields to apply to the medical field. 
+      I am passionate about the growing technology and software field, and hope to eventually integrate my work in these fields and apply my knowledge within the medical field. 
       </p>
       <p>Some of the programming languages and frameworks I've worked with are:</p>
       <ul>
@@ -24,6 +25,7 @@ export default function Home() {
         <li>Express.js</li>
         <li>React.js</li>
       </ul>
+      </div>
     </div>
   );
 }
